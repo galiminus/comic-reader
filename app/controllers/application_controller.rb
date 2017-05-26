338a4c9bb18803ba6950603f0da1e7ba69e44ba6
Victor Goya <goya.victor@gmail.com>
2017-05-26 16:40:26 +0200

    add pundit in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 end
